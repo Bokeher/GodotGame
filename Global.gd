fdc3f1b3c8189e9e38455dbbf58f3873e5e72cf1
Bokeher <bokeher47@gmail.com>
2024-08-06 23:35:35 +0200

    Autosave the clicks on program exit and autoload on program init

--- Global.gd
+++ Global.gd
@@ -1,12 +1,26 @@
 extends Node
 
 var clicks: int = 0;
+const save_path: String = "user://save"
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	load_clicks();
+	pass 
 
+func load_clicks():
+	if !FileAccess.file_exists(save_path): 
+		print("File not found")
+		return
+	
+	var file = FileAccess.open(save_path, FileAccess.READ)
+	clicks = file.get_var()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func save_clicks():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var(clicks)
+	
+	print("saved")
+	
+func _notification(notification):
+	if notification == NOTIFICATION_WM_CLOSE_REQUEST:
+		save_clicks();
