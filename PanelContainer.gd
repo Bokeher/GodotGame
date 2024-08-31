62d8b6878a1df462f4895a355c6f8d0eea9868f7
Bokeher <bokeher47@gmail.com>
2024-08-31 21:28:35 +0200

    Delete unused file

--- PanelContainer.gd
+++
@@ -1,8 +0,0 @@
-extends PanelContainer
-
-func _on_mouse_entered():
-	Popups.ItemPopup(Rect2i(Vector2i(global_position), Vector2i(size)), null)
-
-
-func _on_mouse_exited():
-	Popups.HideItemPopup()
