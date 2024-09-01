9c2c5ba6d19d078638b66597cd6e63578d5f9140
Bokeher <bokeher47@gmail.com>
2024-09-01 21:58:25 +0200

    Load upgrades file

--- Global.gd
+++ Global.gd
@@ -4,6 +4,7 @@ extends Node
 const PATH_SAVE: String = "user://save"
 const PATH_ENEMIES: String = "res://assets/enemies/enemies.json"
 const PATH_STAGES: String = "res://assets/stages.json"
+const PATH_UPGRADES: String = "res://assets/upgrades.json"
 
 # saved vars
 var damage: int = 1
@@ -18,12 +19,16 @@ var enemy_pool = []
 # private vars - use getters to get them
 var _enemies = []
 var _stages = []
+var _upgrades = []
 
 # TODO: decide if need to save curr_enemy
+# TODO: decide if need get_upgrade and get_stage
 
 func _ready():
 	read_stages()
 	read_enemies()
+	read_upgrades()
+	
 	read_savefile()
 	
 	curr_stage = get_stage(max_stage_reached)
@@ -79,3 +84,14 @@ func read_stages():
 
 func get_stage(id: int):
 	return _stages[id - 1].duplicate()
+
+func read_upgrades():
+	if(!FileAccess.file_exists(PATH_UPGRADES)): 
+		print("Upgrades file not found")
+		return
+	
+	var json_to_read = FileAccess.open(PATH_UPGRADES, FileAccess.READ)
+	_upgrades = JSON.parse_string(json_to_read.get_as_text()).upgrades
+
+func get_upgrade(id: int):
+	return _upgrades[id - 1].duplicate()
