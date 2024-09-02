b656eb81a10a5a4fc0f849955bcafe78d0d2ab43
Bokeher <bokeher47@gmail.com>
2024-09-02 23:16:17 +0200

    Create new scene for upgrades

--- Upgrade.gd
+++ Upgrade.gd
@@ -2,14 +2,3 @@ extends Button
 
 func _pressed():
 	$"../..".upgrade(10, 1)
-
-func _on_mouse_entered():
-	Popups.ItemPopup(
-		Rect2i(Vector2i(global_position), 
-		Vector2i(size)), 
-		"Upgrade your weapon", 
-		"+1 Damage \n-10 Gold"
-	)
-
-func _on_mouse_exited():
-	Popups.HideItemPopup()
