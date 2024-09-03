68ffbd8355f256333c218bfb3bbe13f914411f1e
Bokeher <bokeher47@gmail.com>
2024-09-03 20:55:34 +0200

    Update project structure

--- Upgrade.gd
+++ Upgrade.gd
@@ -1,4 +1,13 @@
-extends Button
+extends Control
 
-func _pressed():
-	$"..".upgrade(10, 1)
+func upgrade(cost: int, damage_increase: int):
+	if Global.gold < cost:
+		# TODO: Show to user
+		print("Not enough gold")
+		return
+		
+	Global.gold -= cost
+	Global.damage += damage_increase
+	
+	$"../../Info/GoldAmount".update_gold()
+	$"../../Info/DamageInfo".update_damage()
