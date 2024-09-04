271350c20998d2c89064376b083be8d42fdded8a
Bokeher <bokeher47@gmail.com>
2024-09-04 23:25:56 +0200

    Refactor after changing structure of elements

--- Upgrades.gd
+++ Upgrades.gd
@@ -5,5 +5,8 @@ const upgrade_scene = preload("res://scenes/Upgrade.tscn")
 func _ready():
 	for upgrade in Global._upgrades:
 		var new_upgrade = upgrade_scene.instantiate()
-		new_upgrade.get_node("Background/Effect").text = upgrade.name
+		new_upgrade.get_node("Background/UpgradeName").text = upgrade.name
+		new_upgrade.get_node("Background/UpgradeBonus").text = upgrade.description
+		new_upgrade.get_node("Background/UpgradeButton").text = str(upgrade.cost)
+		
 		$VBoxContainer.add_child(new_upgrade)
