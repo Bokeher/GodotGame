7be40b3f9c560da007c8d020b776589c41e8eab2
Bokeher <bokeher47@gmail.com>
2024-09-08 22:28:12 +0200

    Make upgrade function more versatile

--- Upgrade.gd
+++ Upgrade.gd
@@ -3,14 +3,19 @@ extends Control
 func _ready():
 	$".".set_custom_minimum_size($Background.size)
 
-func upgrade(cost: int, damage_increase: int):
-	if Global.gold < cost:
+func upgrade(upgrade):
+	if(Global.gold < upgrade.cost):
 		# TODO: Show to user
 		print("Not enough gold")
 		return
 		
-	Global.gold -= cost
-	Global.damage += damage_increase
+	Global.gold -= upgrade.cost
+	
+	var id = upgrade.id
+	if(id == 1):
+		Global.damage += upgrade.value
+	#elif(id == 2):
+		
 	
 	$"../../../../Info/GoldAmount".update_gold()
 	$"../../../../Info/DamageInfo".update_damage()
--- UpgradeButton.gd
+++ UpgradeButton.gd
@@ -4,4 +4,4 @@ func _pressed():
 	var upgrade_id = int($"../UpgradeId".text)
 	
 	var upgrade = Global.get_upgrade(upgrade_id)
-	$"../..".upgrade(upgrade.cost, upgrade.value)
+	$"../..".upgrade(upgrade)
