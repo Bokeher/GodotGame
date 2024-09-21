6053810f38b8c289c21a5b82e1772ad2dd6967d8
Bokeher <bokeher47@gmail.com>
2024-09-21 23:17:55 +0200

    Reset button updates gold and damage info after reset

--- Reset.gd
+++ Reset.gd
@@ -4,3 +4,6 @@ func _pressed():
 	Global.player_stats.damage = 1
 	Global.player_stats.gold = 0
 	Global.load_upgrade_stats()
+	
+	$"../Info/GoldAmount".update_gold()
+	$"../Info/DamageInfo".update_damage()
