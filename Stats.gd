9c36e6b7579b8a7f83284308fffd615cd7f7c803
Bokeher <bokeher47@gmail.com>
2024-09-26 22:07:54 +0200

    Add remaining stats to stats tab

--- Stats.gd
+++ Stats.gd
@@ -7,7 +7,14 @@ func _ready():
 
 func update_stats():
 	var damage = Global.player_stats.damage
-	var crit = Global.player_stats.crit
+	var crit =  Global.player_stats.crit * 100
+	var respawn_time = 1.01 - Global.player_stats.speed * 0.01
+	var gold_mult = Global.player_stats.luck + 99
+	var xp_mult = Global.player_stats.wisdom + 99
 	
 	$ColorRect/StatsContainer/Damage.text = "Damage: " + str(damage)
-	$ColorRect/StatsContainer/Crit.text = "Crit chance: " + str(crit * 100) + "%"
+	$ColorRect/StatsContainer/Crit.text = "Crit chance: " + str(crit) + "%"
+	$ColorRect/StatsContainer/RespawnTime.text = "Enemy find time: " + str(respawn_time) + "s"
+	$ColorRect/StatsContainer/GoldMult.text = "Gold multiplier: " + str(gold_mult) + "%"
+	$ColorRect/StatsContainer/XpMult.text = "Xp multiplier: " + str(xp_mult) + "%"
+	
