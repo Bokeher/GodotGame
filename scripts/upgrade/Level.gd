95f336a8402db17dfc08e48958d2ce5f621fb24b
Bokeher <bokeher47@gmail.com>
2024-10-01 19:47:18 +0200

    Show max level of upgrade

--- scripts/upgrade/Level.gd
+++ scripts/upgrade/Level.gd
@@ -2,5 +2,13 @@ extends Label
 
 func update_level():
 	var id: int = int($"../UpgradeId".text)
+	
 	var level: int = Global.upgrade_stats_array[id - 1].level
-	$".".text = "Level " + str(level)
+	var max_level: int = Global.get_upgrade(id).max_level
+	
+	var level_text = "Level " + str(level)
+	
+	if(max_level > 0):
+		level_text += " / " + str(max_level) 
+
+	$".".text = level_text
