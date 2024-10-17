7c5ca46ba24a791d759db473613c8dd9511a2f2e
Bokeher <bokeher47@gmail.com>
2024-10-17 20:31:35 +0200

    Add health regeneration mechanic

--- scripts/game/PlayerHealth.gd
+++ scripts/game/PlayerHealth.gd
@@ -1,6 +1,7 @@
 extends Label
 
 var time_passed = 0
+var regen_time_passed = 0
 
 func _ready() -> void:
 	update_player_health()
@@ -11,10 +12,23 @@ func update_player_health() -> void:
 
 func _process(delta) -> void:
 	time_passed += delta
+	regen_time_passed += delta
 	
 	var attack_time = 1.0
+	
+	var regen_time = 1.0
+	var regen_amount = 1
+	
 	var remaining_time = attack_time - time_passed
 	$"../../Enemy/EnemyAttackTimer".text = str(floor(remaining_time * 10) / 10) + "s"
+	
+	if(regen_time_passed >= regen_time && $"../../ActionButton".curr_state == 0):
+		if(Global.player_stats.health + regen_amount <= Global.player_stats.max_health):
+			Global.player_stats.health += regen_amount
+		
+		update_player_health()
+		$"..".update_player_health_bar()
+		regen_time_passed = 0
 	
 	if time_passed >= attack_time:
 		if(!Global.curr_enemy):
