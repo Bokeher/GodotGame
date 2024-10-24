4e06ced1432f4e3830e45316f4d40ecbb464ebda
Bokeher <bokeher47@gmail.com>
2024-10-24 19:41:27 +0200

    Move PlayerStats class to a separate file

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,28 +1,14 @@
 extends Node
 
-# load classes
+# Load classes
 const UpgradeStats = preload("res://scripts/classes/UpgradeStats.gd")
+const PlayerStats = preload("res://scripts/classes/PlayerStats.gd")
 
-# file paths
+# File paths
 const PATH_SAVE: String = "user://save"
 const PATH_ENEMIES: String = "res://assets/jsons/enemies.json"
 const PATH_STAGES: String = "res://assets/jsons/stages.json"
 const PATH_UPGRADES: String = "res://assets/jsons/upgrades.json"
-
-class PlayerStats:
-	var max_health: int = 10
-	var health: int = 10
-	var regen: int = 2
-	
-	var damage: int = 1
-	var crit: float = 0.00
-	var speed: int = 1
-	var wisdom: int = 1
-	var luck: int = 1
-	
-	var gold: int = 0
-	var max_stage_reached: int = 1
-
 
 # State vars
 var curr_enemy = null
---
+++ scripts/classes/PlayerStats.gd
@@ -0,0 +1,14 @@
+class_name PlayerStats
+
+var max_health: int = 10
+var health: int = 10
+var regen: int = 2
+
+var damage: int = 1
+var crit: float = 0.00
+var speed: int = 1
+var wisdom: int = 1
+var luck: int = 1
+
+var gold: int = 0
+var max_stage_reached: int = 1
