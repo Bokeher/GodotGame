1a1073dc009ea8af92865607a73bfb1d3da5ec0d
Bokeher <bokeher47@gmail.com>
2024-11-06 20:36:45 +0100

    Improve readability of Search.gd

--- scripts/game/Search.gd
+++ scripts/game/Search.gd
@@ -13,13 +13,20 @@ func _pressed() -> void:
 	
 	# run away option - if there's an enemy and state is idle 
 	if(curr_state == 0 && Global.curr_enemy):
-		$"../Enemy".hide_enemy()
 		$"../Enemy/HealthBar".value = 0
 		Global.curr_enemy = null
+		
+		$"../Enemy".hide_enemy()
 		$"../Enemy".update_enemy()
 	
-	# if there's no enemy, loading bar is at 0% (to prevent restarting from 0 on resuming search) and state is searching
-	if(curr_state == 1 && !Global.curr_enemy && $"../Enemy/HealthBar".value == 0):
+	# if no enemy
+	# AND loading bar is at 0% (this allows resuming search instead of starting from 0) 
+	# AND state is searching
+	if (
+		!Global.curr_enemy 
+		&& $"../Enemy/HealthBar".value == 0 
+		&& curr_state == SEARCH_STATE
+	):
 		$"../Enemy/HealthBar".start_filling()
 	
 	update_action_button()
