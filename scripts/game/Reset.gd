c4b57f48fbeefe07801cb7af6137c70b2467223d
Bokeher <bokeher47@gmail.com>
2024-11-09 19:46:38 +0100

    Make 'Reset' reset healthbar and reset entire 'PlayerStats' object instead of a few properites of 'PlayerStats' object

--- scripts/game/Reset.gd
+++ scripts/game/Reset.gd
@@ -1,14 +1,8 @@
 extends Button
 
 func _pressed() -> void:
-	Global.player_stats.damage = 1
-	Global.player_stats.crit = 0
-	Global.player_stats.speed = 1
-	Global.player_stats.wisdom = 1
-	Global.player_stats.luck = 1
-	
-	Global.player_stats.gold = 0
-	Global.player_stats.max_stage_reached = 1
+	Global.player_stats = PlayerStats.new()
+	$"../PlayerHealthBar".update_player_health_bar()
 	
 	Global.load_upgrade_stats()
 	$"../Info/GoldAmount".update_gold()
