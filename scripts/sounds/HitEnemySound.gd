9ae2eace6071d5e8a783f5ba675db2f1cea8f9ac
Bokeher <bokeher47@gmail.com>
2024-11-18 19:16:02 +0100

    Correct the var name (isCrit => is_crit)

--- scripts/sounds/HitEnemySound.gd
+++ scripts/sounds/HitEnemySound.gd
@@ -1,6 +1,6 @@
 extends AudioStreamPlayer
 
-func change_pitch(isCrit: bool = false) -> void:
-	var pitch = randf_range(10, 12) if isCrit else randf_range(0.8, 3)
+func change_pitch(is_crit: bool = false) -> void:
+	var pitch = randf_range(10, 12) if is_crit else randf_range(0.8, 3)
 	
 	$".".pitch_scale = pitch
