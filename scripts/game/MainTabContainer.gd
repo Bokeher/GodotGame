752e9cfd2c80ec3606a53de77ebd5177efa932af
Bokeher <bokeher47@gmail.com>
2024-11-19 20:22:25 +0100

    Prevent out of bounds error when setting tab titles

--- scripts/game/MainTabContainer.gd
+++ scripts/game/MainTabContainer.gd
@@ -4,6 +4,6 @@ const TAB_NAMES = ["Upgrades", "Stats"];
 
 func _ready():
 	# Set custom tab names
-	for i in range($".".get_tab_count()):
+	for i in range(min($".".get_tab_count(), TAB_NAMES.size())):
 		$".".set_tab_title(i, TAB_NAMES[i])
 
