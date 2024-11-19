b9771f21c807920cda3ed59a50712f7ec240551c
Bokeher <bokeher47@gmail.com>
2024-11-19 19:58:39 +0100

    Move script from removed UpgradeList to UpgradeContainer

---
+++ scripts/UpgradeContainer.gd
@@ -0,0 +1,13 @@
+extends ScrollContainer
+
+const upgrade_scene = preload("res://scenes/Upgrade.tscn")
+
+func _ready() -> void:
+	for upgrade in Global._upgrades:
+		var new_upgrade = upgrade_scene.instantiate()
+		new_upgrade.get_node("Background/UpgradeName").text = upgrade.name
+		new_upgrade.get_node("Background/UpgradeBonus").text = upgrade.description
+		new_upgrade.get_node("Background/UpgradeButton").text = str(upgrade.cost)
+		new_upgrade.get_node("Background/UpgradeId").text = str(upgrade.id)
+		
+		$VBoxContainer.add_child(new_upgrade)
