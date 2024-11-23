03d996e7b6a16592b01ebfc40f045da417a399b2
Bokeher <bokeher47@gmail.com>
2024-11-23 20:18:09 +0100

    Add SkillNode

---
+++ scripts/game/skills/SkillNode.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var skill_name = "Skill name"
+var skill_descritpion = "Skill descritpion"
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_texture_button_mouse_entered():
+	Popups.ItemPopup(Rect2i(Vector2i(global_position), Vector2i(size)), skill_name, skill_descritpion)
+
+func _on_texture_button_mouse_exited():
+	Popups.HideItemPopup()
