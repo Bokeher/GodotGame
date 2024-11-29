69e950790d3e9235c342c3b6247e76a6b02574d3
Bokeher <bokeher47@gmail.com>
2024-11-29 20:30:06 +0100

    Remove unnecessary _ready()

--- scripts/game/skills/SkillNode.gd
+++ scripts/game/skills/SkillNode.gd
@@ -3,9 +3,6 @@ extends Control
 var skill_name = "Skill name"
 var skill_descritpion = "Skill descritpion"
 
-func _ready():
-	pass # Replace with function body.
-
 func _on_texture_button_mouse_entered():
 	Popups.ItemPopup(Rect2i(Vector2i(global_position), Vector2i(size)), skill_name, skill_descritpion)
 
