3285e131bd422125ff79a6c30b091fd2984dc6f8
Bokeher <bokeher47@gmail.com>
2024-12-03 20:34:40 +0100

    Clean the popup code

--- scripts/game/Popups.gd
+++ scripts/game/Popups.gd
@@ -1,15 +1,13 @@
 extends Control
 
-func ItemPopup(parent: Rect2i, text: String, description: String) -> void:
-	var mouse_position = get_viewport().get_mouse_position()
-
+func SkillPopup(text: String, description: String) -> void:
 	%PopupTitle.text = text
 	%PopupDescription.text = description
 
 	# TODO: somehow make this appear to the left of the $MainTabContainer without this hardcoded
-	var offset = Vector2i(455, 30)
+	var popup_position = Vector2i(455, 30)
 
-	%ItemPopup.popup(Rect2i(offset, %ItemPopup.size))
+	%SkillPopup.popup(Rect2i(popup_position, %SkillPopup.size))
 
-func HideItemPopup() -> void:
-	%ItemPopup.hide()
+func HideSkillPopup() -> void:
+	%SkillPopup.hide()
--- scripts/game/skills/SkillNode.gd
+++ scripts/game/skills/SkillNode.gd
@@ -4,7 +4,7 @@ var skill_name = "Skill name"
 var skill_descritpion = "Skill descritpion"
 
 func _on_texture_button_mouse_entered():
-	Popups.ItemPopup(Rect2i(Vector2i(global_position), Vector2i(size)), skill_name, skill_descritpion)
+	Popups.SkillPopup(skill_name, skill_descritpion)
 
 func _on_texture_button_mouse_exited():
-	Popups.HideItemPopup()
+	Popups.HideSkillPopup()
