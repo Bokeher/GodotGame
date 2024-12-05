f6d803cce1541567dbeca6c825a2ff186a940fe0
Bokeher <bokeher47@gmail.com>
2024-12-05 20:42:59 +0100

    Add leveling skills

--- scripts/game/skills/SkillNode.gd
+++ scripts/game/skills/SkillNode.gd
@@ -2,9 +2,27 @@ extends Control
 
 var skill_name = "Skill name"
 var skill_descritpion = "Skill descritpion"
+var skill_level = 0
+var max_level = 5
+
+func _ready():
+	update_level_label()
 
 func _on_texture_button_mouse_entered():
 	Popups.SkillPopup(skill_name, skill_descritpion)
 
 func _on_texture_button_mouse_exited():
 	Popups.HideSkillPopup()
+
+func _on_texture_button_pressed():
+	level_up_skill()
+
+func level_up_skill():
+	if(skill_level >= max_level):
+		return
+	
+	skill_level += 1
+	update_level_label()
+
+func update_level_label():
+	$Label.text = str(skill_level) + " / " + str(max_level)
