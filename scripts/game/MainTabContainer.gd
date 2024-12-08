5a5a26c34c7de0af89ffaf6b9b12dcdb1d0b6908
Bokeher <bokeher47@gmail.com>
2024-12-08 19:42:41 +0100

    Remove unnecessary semicolon

--- scripts/game/MainTabContainer.gd
+++ scripts/game/MainTabContainer.gd
@@ -1,9 +1,8 @@
 extends TabContainer
 
-const TAB_NAMES = ["Upgrades", "Stats", "Skills"];
+const TAB_NAMES = ["Upgrades", "Stats", "Skills"]
 
 func _ready():
 	# Set custom tab names
 	for i in range(min($".".get_tab_count(), TAB_NAMES.size())):
 		$".".set_tab_title(i, TAB_NAMES[i])
-
