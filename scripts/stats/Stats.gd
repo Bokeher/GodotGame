331926d806f7546fcb2a059c0d645346d8382f9f
Bokeher <bokeher47@gmail.com>
2024-12-17 20:38:54 +0100

    Display xp and level in Stats tab

--- scripts/stats/Stats.gd
+++ scripts/stats/Stats.gd
@@ -5,7 +5,9 @@ var stats_texts = {
 	"CritChance": "Crit chance",
 	"RespawnTime": "Time to find enemy",
 	"GoldMultiplier": "Gold multiplier",
-	"XpMultiplier": "XP multiplier"
+	"XpMultiplier": "XP multiplier",
+	"XpAmount": "XP",
+	"Level": "Level"
 }
 
 func _ready() -> void:
@@ -17,12 +19,16 @@ func update_stats() -> void:
 	var respawn_time = Global.player_stats.calc_respawn_time()
 	var gold_mult = Global.player_stats.calc_gold_mult()
 	var xp_mult = Global.player_stats.calc_xp_mult()
+	var xp = Global.player_stats.xp
+	var level = Global.player_stats.level
 	
 	update_stat_text("Damage", damage)
 	update_stat_text("CritChance", crit, "%")
 	update_stat_text("RespawnTime", respawn_time, "s")
 	update_stat_text("GoldMultiplier", gold_mult, "%")
 	update_stat_text("XpMultiplier", xp_mult, "%")
+	update_stat_text("XpAmount", xp, " / 100") #TODO: remove this hardcoded text
+	update_stat_text("Level", level)
 
 func update_stat_text(stat_name: String, value: Variant, suffix: String = "") -> void:
 	var text_node = $ColorRect/StatsContainer.get_node(stat_name)
