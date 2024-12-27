0728001ffe72951d99b0b8ad3fdbf59e56ad1cc0
Bokeher <bokeher47@gmail.com>
2024-12-27 20:04:57 +0100

    Fix errors caused by refactoring

--- scripts/game/Reset.gd
+++ scripts/game/Reset.gd
@@ -5,5 +5,5 @@ func _pressed() -> void:
 	$"../PlayerHealthBar".update_player_health_bar()
 	
 	Global.load_upgrade_stats()
-	$"../MainTabContainer/Panel/GoldAmount".update_gold()
+	$"../MainTabContainer/UpgradesPanel/GoldAmount".update_gold()
 	$"../MainTabContainer/Stats".update_stats()
