eedb08496a7ff996d0debe3cc6fe2bd04d13ca8c
Bokeher <bokeher47@gmail.com>
2024-12-31 21:52:54 +0100

    Add methods for reading and saving skills

--- scripts/classes/Skill.gd
+++ scripts/classes/Skill.gd
@@ -12,3 +12,24 @@ func _init(_id: int, _name: String, _description: String, _image_url: String, _m
 	description = _description
 	image_url = _image_url
 	max_level = _max_level
+
+# Convert instance to dictionary for saving
+func to_dict() -> Dictionary:
+	return {
+		"id": id,
+		"name": name,
+		"description": description,
+		"image_url": image_url,
+		"max_level": max_level
+	}
+
+# Static method to create an instance from a dictionary (used when reading from a save file)
+static func from_dict(data: Dictionary) -> Skill:
+	return Skill.new(
+		data.get("id", -1),
+		data.get("name", ""),
+		data.get("description", ""),
+		data.get("image_url", ""),
+		data.get("max_level", -1)
+	)
+
