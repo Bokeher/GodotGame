bb975427fb790a502f96956a61d21ffd42eed406
Bokeher <bokeher47@gmail.com>
2024-12-31 21:51:47 +0100

    Change Skill class to match json counterpart

--- scripts/classes/Skill.gd
+++ scripts/classes/Skill.gd
@@ -3,14 +3,12 @@ class_name Skill
 var id: int
 var name: String
 var description: String
+var image_url: String
 var max_level: int
-var curr_level: int
-var img_url: String
 
-func _init(_id: int, _name: String, _description: String, _max_level: int, _curr_level: int, _img_url: String):
+func _init(_id: int, _name: String, _description: String, _image_url: String, _max_level: int):
 	id = _id
 	name = _name
 	description = _description
+	image_url = _image_url
 	max_level = _max_level
-	curr_level = _curr_level
-	img_url = _img_url
