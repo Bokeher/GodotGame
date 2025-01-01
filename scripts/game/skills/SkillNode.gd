3d2503a6cb34f286a230211ce5e7fc6cc4aad6ed
Bokeher <bokeher47@gmail.com>
2025-01-01 20:01:05 +0100

    Use Skill object for SkillNodes. Use data stored Skills.json instead of hardcoded values

--- scripts/game/skills/SkillNode.gd
+++ scripts/game/skills/SkillNode.gd
@@ -1,17 +1,18 @@
 extends Control
 
-var skill_name = "Skill name"
-var skill_descritpion = "Skill descritpion"
 var skill_level = 0
-var max_level = 5
+
+var skill: Skill
 
 func _ready():
-	skill_name = $".".get_meta("skill_name")
-	skill_descritpion = $".".get_meta("skill_descritpion")
+	var id = $".".get_meta("id")
+	
+	skill = Global._skills[id - 1]
+	
 	update_level_label()
 
 func _on_texture_button_mouse_entered():
-	Popups.SkillPopup(skill_name, skill_descritpion, skill_level)
+	Popups.SkillPopup(skill.name, skill.description, skill_level)
 
 func _on_texture_button_mouse_exited():
 	Popups.HideSkillPopup()
@@ -20,11 +21,11 @@ func _on_texture_button_pressed():
 	level_up_skill()
 
 func level_up_skill():
-	if(skill_level >= max_level):
+	if(skill_level >= skill.max_level):
 		return
 	
 	skill_level += 1
 	update_level_label()
 
 func update_level_label():
-	$SkillLevelLabel.text = str(skill_level) + " / " + str(max_level)
+	$SkillLevelLabel.text = str(skill_level) + " / " + str(skill.max_level)
