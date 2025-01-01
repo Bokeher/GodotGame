cf9331a193dfda5ea8b16e91f635f05ae8abc9a5
Bokeher <bokeher47@gmail.com>
2025-01-01 20:12:47 +0100

    Fix popup offset because previous commit somehow changed it

--- scripts/game/SkillPopup.gd
+++ scripts/game/SkillPopup.gd
@@ -5,7 +5,7 @@ func SkillPopup(text: String, description: String, skill_level: int) -> void:
 	%PopupDescription.text = description
 	%PopupSkillLevel.text = "Level " + str(skill_level)
 
-	var popup_position = Global.MAIN_TAB_CONTAINER_POSITION + Vector2i(-105, 31) # +30 on y is 1 pixel off
+	var popup_position = Global.MAIN_TAB_CONTAINER_POSITION + Vector2i(-115, 31) # +30 on y is 1 pixel off
 
 	%SkillPopup.popup(Rect2i(popup_position, %SkillPopup.size))
 
