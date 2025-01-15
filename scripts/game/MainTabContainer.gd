6078e7a96a0babc9337d609a0b50cbb5e5bcaa8b
Bokeher <bokeher47@gmail.com>
2025-01-15 20:34:24 +0100

    Add shortcuts to switch between tabs

--- scripts/game/MainTabContainer.gd
+++ scripts/game/MainTabContainer.gd
@@ -9,3 +9,13 @@ func _ready():
 	# Set custom tab names
 	for i in range(min($".".get_tab_count(), TAB_NAMES.size())):
 		$".".set_tab_title(i, TAB_NAMES[i])
+
+func _process(delta):
+	if Input.is_action_just_pressed("JumpUpgrades"):
+		$".".current_tab = 0
+	if Input.is_action_just_pressed("JumpStats"):
+		$".".current_tab = 1
+	if Input.is_action_just_pressed("JumpSkills"):
+		$".".current_tab = 2
+	if Input.is_action_just_pressed("JumpInventory"):
+		$".".current_tab = 3
