c0d994e2612b2eb9095e86f53c61e03c8bce9e03
Bokeher <bokeher47@gmail.com>
2025-01-22 20:44:28 +0100

    Add loot table to Enemy class

--- scripts/classes/Enemy.gd
+++ scripts/classes/Enemy.gd
@@ -7,8 +7,9 @@ var gold_reward: int
 var image_path: String
 var damage: int
 var xp_reward: int
+var loot_table: Array
 
-func _init(_id: int, _name: String, _health: int, _gold_reward: int, _image_path: String, _damage: int, _xp_reward: int):
+func _init(_id: int, _name: String, _health: int, _gold_reward: int, _image_path: String, _damage: int, _xp_reward: int, _loot_table: Array = []):
 	id = _id
 	name = _name
 	health = _health
@@ -16,6 +17,7 @@ func _init(_id: int, _name: String, _health: int, _gold_reward: int, _image_path
 	image_path = _image_path
 	damage = _damage
 	xp_reward = _xp_reward
+	loot_table = _loot_table
 
 # Used in saving
 func to_dict() -> Dictionary:
@@ -26,7 +28,8 @@ func to_dict() -> Dictionary:
 		"gold_reward": gold_reward,
 		"image_path": image_path,
 		"damage": damage,
-		"xp_reward": xp_reward
+		"xp_reward": xp_reward,
+		"loot_table": loot_table
 	}
 
 # Used in reading from savefile
@@ -38,17 +41,31 @@ static func from_dict(data: Dictionary) -> Enemy:
 		data.get("gold_reward", 0),
 		data.get("image_path", ""),
 		data.get("damage", 0),
-		data.get("xp_reward", 1)
+		data.get("xp_reward", 1),
+		#data.get("loot_table", [])
 	)
 	
 	return instance
 
 func duplicate() -> Enemy:
-	return Enemy.new(id, name, health, gold_reward, image_path, damage, xp_reward)
+	return Enemy.new(id, name, health, gold_reward, image_path, damage, xp_reward, loot_table.duplicate(true))
+
+# Roll loot from the loot table
+func get_loot() -> Array:
+	var dropped_items = []
+	var cumulative_chance = 0.0
+	var random_value = randf()
+
+	for loot in loot_table:
+		cumulative_chance += loot["drop_chance"]
+		if random_value < cumulative_chance:
+			dropped_items.append(loot["item"])
+			random_value = randf()  # Roll again for potential multiple drops
+
+	return dropped_items
 
 static func set_new_random_enemy() -> void:
 	var cumulative_chance = 0.0
-	
 	var random_value = randf()
 	
 	for enemy in Global.curr_stage.enemies:
