6b465474310bb03bac0938df6f48c877dc382f43
Bokeher <bokeher47@gmail.com>
2025-02-09 21:00:25 +0100

    Display xp progress

--- scripts/player/PlayerXpBar.gd
+++ scripts/player/PlayerXpBar.gd
@@ -7,5 +7,7 @@ func update_xp_bar():
 	var need_xp = 100 # TODO: replace this hardcoded value
 	var curr_xp = Global.player_stats.xp
 	
+	$PlayerXpProgressLabel.text = str(Global.player_stats.xp) + " / " + str(need_xp)
+	
 	$".".max_value = need_xp
 	$".".value = curr_xp
