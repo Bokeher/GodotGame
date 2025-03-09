65a9cf125a3d92a81889a12dfd4d5f9eff2d7751
Bokeher <bokeher47@gmail.com>
2025-03-09 20:13:36 +0100

    Remove redundant signal

--- scripts/bestiary/BestiaryListItem.gd
+++ scripts/bestiary/BestiaryListItem.gd
@@ -7,17 +7,13 @@ func set_enemy(enemy_id: int):
 	var image_path = Global.enemies[enemy_id - 1].image_path
 	$TextureButton.texture_normal = load(image_path)
 
-func _on_texture_button_pressed():
-	var enemy_id = $".".get_meta("enemy_id")
-	
-	Global.curr_bestiary_enemy_id = enemy_id
-	$"../../..".update_selected_bestiary()
-
-# TODO: make some border to make this visible
 func _on_texture_button_focus_entered():
 	$ColorRect.color = Color(0.30, 0.30, 0.30)
-	_on_texture_button_pressed()
+	
+	var enemy_id = $".".get_meta("enemy_id")
+	Global.curr_bestiary_enemy_id = enemy_id
+	
+	$"../../..".update_selected_bestiary()
 
 func _on_texture_button_focus_exited():
 	$ColorRect.color = Color(0.21, 0.21, 0.21)
-	pass # Replace with function body.
