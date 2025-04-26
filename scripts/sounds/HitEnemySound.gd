5982d8bec37b011042d47bcb1bcef8e754724f83
Bokeher <bokeher47@gmail.com>
2025-04-26 20:50:49 +0200

    Use regular 'if' instead of ternary operator

--- scripts/sounds/HitEnemySound.gd
+++ scripts/sounds/HitEnemySound.gd
@@ -1,6 +1,8 @@
 extends AudioStreamPlayer
 
 func change_pitch(is_crit: bool = false) -> void:
-	var pitch = randf_range(10, 12) if is_crit else randf_range(0.8, 3)
+	var pitch = randf_range(0.8, 3)
+	if is_crit:
+		pitch = randf_range(10, 12)
 	
 	$".".pitch_scale = pitch
