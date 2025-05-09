2fcfab1ae3e3de76845a84c6d356df5e336d83d6
Bokeher <bokeher47@gmail.com>
2025-05-09 20:40:31 +0200

    Add comments to BestiaryItem

--- scripts/bestiary/BestiaryItem.gd
+++ scripts/bestiary/BestiaryItem.gd
@@ -9,6 +9,9 @@ func update_bestiary_item() -> void:
 	if !bestiaryEntry:
 		return
 	
+	# TODO: reduce into one loop, 
+	# TODO: preserve position of dropped items (same in bestiary)
+	
 	$EnemyName.text = enemy.name
 	$EnemyImage.texture = load(enemy.image_path)
 	$DefeatCount.text = "Times defeated: " + str(bestiaryEntry.times_slayed)
@@ -17,30 +20,38 @@ func update_bestiary_item() -> void:
 	$EnemyStats/XpReward.text = "Xp reward: " + str(enemy.xp_reward)
 	$EnemyStats/GoldReward.text = "Gold reward: " + str(enemy.gold_reward)
 	
-	# <id, has_dropped: bool>
-	var drops = bestiaryEntry.items_dropped
+	# Dictionary<id: int, has_dropped: bool>
+	var drops: Dictionary = bestiaryEntry.items_dropped
 	
+	# Empty loot table
 	for child in $Drops/LootTable.get_children():
 		$Drops/LootTable.remove_child(child)
 	
+	# Add items
 	for item_id in drops:
-		if(drops[item_id]):
-			var new_loot_item = loot_item_scene.instantiate()
-			
-			var item_image = new_loot_item.get_node("./LootImage")
-			var image_path = Global.items[item_id - 1].image_path
-			item_image.texture = load(image_path)
-			
-			$Drops/LootTable.add_child(new_loot_item)
-			#TODO: Add popups
+		# Skip iteration if item has not been dropped before
+		if(!drops[item_id]):
+			continue
+		
+		var new_loot_item = loot_item_scene.instantiate()
+		
+		var item_image = new_loot_item.get_node("./LootImage")
+		var image_path = Global.items[item_id - 1].image_path
+		item_image.texture = load(image_path)
+		
+		$Drops/LootTable.add_child(new_loot_item)
+		#TODO: Add popups
 	
 	var childs = $Drops/LootTable.get_children()
 	var loot_table = enemy.loot_table
 	
+	# Add items that has not been dropped as uknown.png
 	for loot_table_element in loot_table:
+		# All items dropped => end loop
 		if(loot_table.size() == childs.size()): 
 			break
 		
+		# If item dropped => skip iteration
 		if(drops.has(loot_table_element["item_id"])): 
 			continue
 		
