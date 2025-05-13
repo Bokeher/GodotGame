26a006d2df1e88c12a03a02623b9729da30365a4
Bokeher <bokeher47@gmail.com>
2025-05-13 20:08:56 +0200

    Add slot_type metadata

--- EquippedInventorySlot.gd
+++ EquippedInventorySlot.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var this_slot_id: int = $".".get_meta("id")
+@onready var this_slot_type: String = $".".get_meta("slot_type")
 
 func focus() -> void:
 	$Background.color = Color(0.30, 0.30, 0.30)
