8c324f89ef226633c4059be78449d524a0894084
Bokeher <bokeher47@gmail.com>
2025-05-19 20:26:57 +0200

    Remove 'this' in var names

--- scripts/inventory/EquippedInventorySlot.gd
+++ scripts/inventory/EquippedInventorySlot.gd
@@ -1,8 +1,8 @@
 extends Control
 
-@onready var this_slot_id: int = $".".get_meta("id")
-@onready var this_slot_type: int = $".".get_meta("slot_type")
-@onready var this_slected_item_id: int = $".".get_meta("selected_item_id")
+@onready var slot_id: int = $".".get_meta("id")
+@onready var slot_type: int = $".".get_meta("slot_type")
+@onready var selected_item_id: int = $".".get_meta("selected_item_id")
 
 func _ready() -> void:
 	update()
@@ -16,23 +16,23 @@ func unfocus() -> void:
 	$Background.color = Color(0.21, 0.21, 0.21)
 
 func _on_slot_texture_pressed() -> void:
-	Global.inventory_filter = this_slot_type
-	Global.selected_equip_slot_id = this_slot_id
+	Global.inventory_filter = slot_type
+	Global.selected_equip_slot_id = slot_id
 	$"../../..".select_artifact_slot()
 	$"../../..".update_inventory()
 
 func update():
-	if this_slot_id == -1 or this_slot_type == -1:
+	if slot_id == -1 or slot_type == -1:
 		print("WARNING! Default equipped inventory slot")
 	
 	var texture_path: String = "res://assets/sprites/unknown.png"
-	if this_slot_type == Enums.InventoryType.ARTIFACT:
+	if slot_type == Enums.InventoryType.ARTIFACT:
 		texture_path = "res://assets/sprites/items/slots/artifact_slot.png"
-	elif this_slot_type == Enums.InventoryType.CHEST:
+	elif slot_type == Enums.InventoryType.CHEST:
 		texture_path = "res://assets/sprites/items/slots/chest_slot.png"
-		
+	
 	$SlotTexture.texture_normal = load(texture_path)
 
 func change_item(item_id) -> void:
-	this_slot_id = item_id
+	selected_item_id = item_id
 	update()
