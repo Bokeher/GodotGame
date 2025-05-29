1c9a594e554a257cf7abb6557501e682c7819267
Bokeher <bokeher47@gmail.com>
2025-05-29 21:11:43 +0200

    Add get_class_texture() to Enums.gd

--- scripts/Enums.gd
+++ scripts/Enums.gd
@@ -13,6 +13,13 @@ enum InventoryType {
 	CHEST = 2
 }
 
+const CLASS_TEXTURES := {
+	Classes.WARRIOR: "res://assets/sprites/classes/warrior.png",
+	Classes.UMBRAL_REAVER: "res://assets/sprites/classes/umral_reaver.png",
+	Classes.LUCKSWORN: "res://assets/sprites/classes/lucksworn.png",
+	Classes.KENSEI: "res://assets/sprites/classes/kensei.png"
+}
+
 const INVENTORY_TEXTURES := {
 	InventoryType.ARTIFACT: "res://assets/sprites/items/slots/artifact_slot.png",
 	InventoryType.CHEST: "res://assets/sprites/items/slots/chest_slot.png"
@@ -20,3 +27,6 @@ const INVENTORY_TEXTURES := {
 
 func get_inventory_type_texture(slot_type: int) -> String:
 	return INVENTORY_TEXTURES.get(slot_type, "res://assets/sprites/unknown.png")
+
+func get_class_texture(class_type: int) -> String:
+	return CLASS_TEXTURES.get(class_type, "res://assets/sprites/classes/unknown.png")
