5e2595f6492dae73a69d22eed9ee23ca838a868e
Bokeher <bokeher47@gmail.com>
2025-06-07 19:23:50 +0200

    Improve maintainability of Enums.gd file

--- scripts/Enums.gd
+++ scripts/Enums.gd
@@ -13,19 +13,25 @@ enum InventoryType {
 	CHEST = 2
 }
 
+const UNKNOWN_PNG_PATH: String = "res://assets/sprites/classes/unknown.png"
+
+# Dir paths
+const CLASS_TEXTURES_DIR_PATH: String = "res://assets/sprites/classes/"
+const INVENTORY_TEXTURES_DIR_PATH: String = "res://assets/sprites/items/slots/"
+const CLASSES_DIR_PATH: String = "res://assets/jsons/classes/"
+
 const CLASS_TEXTURES := {
-	Classes.WARRIOR: "res://assets/sprites/classes/warrior.png",
-	Classes.UMBRAL_REAVER: "res://assets/sprites/classes/umral_reaver.png",
-	Classes.LUCKSWORN: "res://assets/sprites/classes/lucksworn.png",
-	Classes.KENSEI: "res://assets/sprites/classes/kensei.png"
+	Classes.WARRIOR: CLASS_TEXTURES_DIR_PATH + "warrior.png",
+	Classes.UMBRAL_REAVER: CLASS_TEXTURES_DIR_PATH + "umral_reaver.png",
+	Classes.LUCKSWORN: CLASS_TEXTURES_DIR_PATH + "lucksworn.png",
+	Classes.KENSEI: CLASS_TEXTURES_DIR_PATH + "kensei.png"
 }
 
 const INVENTORY_TEXTURES := {
-	InventoryType.ARTIFACT: "res://assets/sprites/items/slots/artifact_slot.png",
-	InventoryType.CHEST: "res://assets/sprites/items/slots/chest_slot.png"
+	InventoryType.ARTIFACT: INVENTORY_TEXTURES_DIR_PATH + "artifact_slot.png",
+	InventoryType.CHEST: INVENTORY_TEXTURES_DIR_PATH + "chest_slot.png"
 }
 
-const CLASSES_DIR_PATH: String = "res://assets/jsons/classes/"
 const CLASS_JSON_PATH := {
 	Classes.WARRIOR: CLASSES_DIR_PATH + "warrior.json",
 	Classes.UMBRAL_REAVER: CLASSES_DIR_PATH + "umbral_reaver.json",
@@ -37,7 +43,7 @@ func get_class_json_path(class_type: int) -> String:
 	return CLASS_JSON_PATH.get(class_type, null)
 
 func get_inventory_type_texture(slot_type: int) -> String:
-	return INVENTORY_TEXTURES.get(slot_type, "res://assets/sprites/unknown.png")
+	return INVENTORY_TEXTURES.get(slot_type, UNKNOWN_PNG_PATH)
 
 func get_class_texture(class_type: int) -> String:
-	return CLASS_TEXTURES.get(class_type, "res://assets/sprites/classes/unknown.png")
+	return CLASS_TEXTURES.get(class_type, UNKNOWN_PNG_PATH)
