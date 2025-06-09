305b13cadee06793ba7db9dd96939c6c3d168519
Bokeher <bokeher47@gmail.com>
2025-06-09 21:06:27 +0200

    Fix naming to match project convention

--- scripts/bestiary/BestiaryItem.gd
+++ scripts/bestiary/BestiaryItem.gd
@@ -4,21 +4,21 @@ const loot_item_scene = preload("res://scenes/bestiary/BestiaryLootItem.tscn")
 
 func update_bestiary_item() -> void:
 	var enemy: Enemy = Global.enemies[Global.curr_bestiary_enemy_id - 1]
-	var bestiaryEntry: BestiaryEntry = Global.bestiary.get_entry(enemy.id)
+	var bestiary_entry: BestiaryEntry = Global.bestiary.get_entry(enemy.id)
 	
-	if !bestiaryEntry:
+	if !bestiary_entry:
 		return
 	
 	$EnemyName.text = enemy.name
 	$EnemyImage.texture = load(enemy.image_path)
-	$DefeatCount.text = "Times defeated: " + str(bestiaryEntry.times_slayed)
+	$DefeatCount.text = "Times defeated: " + str(bestiary_entry.times_slayed)
 	$EnemyStats/Health.text = "Health: " + str(enemy.health)
 	$EnemyStats/Damage.text = "Damage: " + str(enemy.damage)
 	$EnemyStats/XpReward.text = "Xp reward: " + str(enemy.xp_reward)
 	$EnemyStats/GoldReward.text = "Gold reward: " + str(enemy.gold_reward)
 	
 	# Dictionary<id: int, has_dropped: bool>
-	var drops: Dictionary = bestiaryEntry.items_dropped
+	var drops: Dictionary = bestiary_entry.items_dropped
 	
 	# Empty loot table
 	for child in $Drops/LootTable.get_children():
