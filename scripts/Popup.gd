fc21d66db487290969ad106bebace71eefa47499
Bokeher <bokeher47@gmail.com>
2025-07-11 21:23:35 +0200

    Add delayed show and hide to global Popup

--- scripts/Popup.gd
+++ scripts/Popup.gd
@@ -2,14 +2,41 @@ extends Control
 
 @onready var name_ = $Popup/Panel/VBoxContainer/Name
 @onready var description = $Popup/Panel/VBoxContainer/Description
+@onready var popup_panel = $Popup
+
+var show_timer = Timer.new()
+var hide_timer = Timer.new()
+var pending_name = ""
+var pending_description = ""
+var rect = Rect2i(315, 31, 0, 0)
+
+func _ready():
+ # Add timers for delayed actions to prevent hiding after showing (happening when elements are really close to each other)
+	add_child(show_timer)
+	show_timer.one_shot = true
+	show_timer.wait_time = 0.05
+	show_timer.timeout.connect(_show_popup)
+	
+	add_child(hide_timer)
+	hide_timer.one_shot = true
+	hide_timer.wait_time = 0.1
+	hide_timer.timeout.connect(_hide_popup)
 
 func popup(popup_name: String, popup_description: String) -> void:
-	var rect = Rect2i(315, 31, 0, 0)
+	pending_name = popup_name
+	pending_description = popup_description
 	
-	name_.text = popup_name
-	description.text = popup_description
-	
-	$Popup.popup(rect)
+	hide_timer.stop()
+	show_timer.start()
 
 func hide_() -> void:
-	$Popup.hide()
+	show_timer.stop()
+	hide_timer.start()
+
+func _show_popup():
+	name_.text = pending_name
+	description.text = pending_description
+	popup_panel.popup(rect)
+
+func _hide_popup():
+	popup_panel.hide()
