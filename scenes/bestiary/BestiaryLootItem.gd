0b97c05cf63b27bd3c39a3746e9d06cb2fb28ae5
Bokeher <bokeher47@gmail.com>
2025-07-12 21:23:08 +0200

    Use global popup in BestiaryLootItem instead of a custom one

--- scenes/bestiary/BestiaryLootItem.gd
+++ scenes/bestiary/BestiaryLootItem.gd
@@ -1,14 +1,12 @@
 extends Control
 
-@onready var popup_panel = $Popup
 @onready var item_id = $".".get_meta("item_id")
-@onready var pos = $".".get_global_rect().position
+@onready var popup = get_node("/root/Game/Popup")
 
 func _on_panel_mouse_entered() -> void:
 	var item = Global.items[item_id - 1]
-	$Popup/Panel/VBoxContainer/ItemName.text = item.name
-	$Popup/Panel/VBoxContainer/Description.text = item.description
-	popup_panel.popup(Rect2i(315, 31, 0, 0))
+	
+	popup.popup(item.name, item.description)
 
 func _on_panel_mouse_exited() -> void:
-	popup_panel.hide()
+	popup.hide_()
