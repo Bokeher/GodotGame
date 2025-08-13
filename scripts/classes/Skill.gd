1769bf6cb33b3317b66fc93868077994f094daac
Bokeher <bokeher47@gmail.com>
2025-08-13 21:43:30 +0200

    Add grid_position to Skill.gd class

--- scripts/classes/Skill.gd
+++ scripts/classes/Skill.gd
@@ -7,8 +7,9 @@ var image_path: String
 var level: int
 var max_level: int
 var requirement_ids: Array[int]
+var grid_position: Array[int]
 
-func _init(_id: int, _name: String, _description: String, _image_path: String, _level: int, _max_level: int, _requirement_ids: Array[int]):
+func _init(_id: int, _name: String, _description: String, _image_path: String, _level: int, _max_level: int, _requirement_ids: Array[int], _grid_position: Array[int]):
 	id = _id
 	name = _name
 	description = _description
@@ -16,9 +17,11 @@ func _init(_id: int, _name: String, _description: String, _image_path: String, _
 	level = _level
 	max_level = _max_level
 	requirement_ids = _requirement_ids
+	grid_position = _grid_position
 
 # Convert instance to dictionary for saving
 func to_dict() -> Dictionary:
+	print(grid_position)
 	return {
 		"id": id,
 		"name": name,
@@ -26,17 +29,26 @@ func to_dict() -> Dictionary:
 		"image_path": image_path,
 		"level": level,
 		"max_level": max_level,
-		"requirement_ids": requirement_ids
+		"requirement_ids": requirement_ids,
+		"grid_position": grid_position
 	}
 
 # Static method to create an instance from a dictionary (used when reading from a save file)
 static func from_dict(data: Dictionary) -> Skill:
 	#	Numbers in arrays are stored as float (godot thing)
 	var requirement_ids_float: Array = data.get("requirement_ids", [])
-	
+
 	var requirement_ids_int: Array[int] = []
 	for requirement_id in requirement_ids_float:
 		requirement_ids_int.append(int(requirement_id))
+	
+	var grid_position_float: Array = data.get("requirement_ids", [0, 0])
+	var grid_position_int: Array[int] = [0, 0]
+	if grid_position_float.size() == 2:
+		grid_position_int = [
+			int(grid_position_float[0]),
+			int(grid_position_float[1])
+		]
 	
 	return Skill.new(
 		data.get("id", -1),
@@ -45,5 +57,6 @@ static func from_dict(data: Dictionary) -> Skill:
 		data.get("image_path", ""),
 		data.get("level", -1),
 		data.get("max_level", -1),
-		requirement_ids_int
+		requirement_ids_int,
+		grid_position_int,
 	)
