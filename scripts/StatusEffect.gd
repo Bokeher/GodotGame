4b4eae302b67ceb941ee170e508002ffc843ad5e
Bokeher <bokeher47@gmail.com>
2025-11-08 20:45:55 +0100

    Add StatusEffect.gd

---
+++ scripts/StatusEffect.gd
@@ -0,0 +1,3 @@
+extends Control
+
+@onready var status_id: int = $".".get_meta("status_id")
---
+++ scripts/classes/StatusEffect.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name StatusEffect
+
+@export var id: int
+@export var name: String
+@export var description: String
+@export var duration: float
+@export var img_path: String
