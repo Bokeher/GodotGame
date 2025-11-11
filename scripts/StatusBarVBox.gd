ef9445ddad831586fa60aec7aa79f8327990c41c
Bokeher <bokeher47@gmail.com>
2025-11-11 20:57:31 +0100

    Implement adding status by status_id

--- scripts/StatusBarVBox.gd
+++ scripts/StatusBarVBox.gd
@@ -1,5 +1,7 @@
 extends VBoxContainer
 
+const status_effect_scene = preload("res://scenes/StatusEffect.tscn")
+
 func remove_status(status_id: int) -> void:
 	var childs: Array = get_children()
 	
@@ -7,3 +9,11 @@ func remove_status(status_id: int) -> void:
 		if child.status.id == status_id:
 			remove_child(child)
 			child.queue_free()
+
+func add_status(status_id: int) -> void:
+	var status: StatusEffect = Global.get_status(status_id)
+	
+	var new_status_scene := status_effect_scene.instantiate()
+	new_status_scene.status = status
+	
+	add_child(new_status_scene)
