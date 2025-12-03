841982fec83979c4ac399f06d4f99c7d104473ba
Bokeher <bokeher47@gmail.com>
2025-12-03 20:55:58 +0100

    Add StageData and SpawnEntry resource classes

---
+++ scripts/classes/resource_classes/SpawnEntry.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name SpawnEntry
+
+@export var enemy: EnemyData
+@export_range(0.0, 1.0, 0.001) var spawn_chance: float = 0.1
---
+++ scripts/classes/resource_classes/StageData.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name StageData
+
+@export var id: int
+@export var name: String
+@export var texture: Texture2D
+@export var spawn_entries: Array[SpawnEntry]
