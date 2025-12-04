6e52a7a43791e646cdb0b9426d783f03c2efb875
Bokeher <bokeher47@gmail.com>
2025-12-04 20:09:38 +0100

    Load resources recursively to allow item categories in subfolders

--- scripts/classes/databases/ResourceLoaderHelper.gd
+++ scripts/classes/databases/ResourceLoaderHelper.gd
@@ -3,28 +3,35 @@ class_name ResourceLoaderHelper # Helper since godot alrdy has ResourceLoader
 
 static func load_folder_to_dict(path: String, key_property := "id") -> Dictionary:
 	var result: Dictionary = {}
+	_scan_dir_recursive(path, key_property, result)
+	return result
+
+static func _scan_dir_recursive(path: String, key_property: String, result: Dictionary) -> void:
 	var dir := DirAccess.open(path)
-	
 	if dir == null:
 		push_error("Can't open directory: %s" % path)
-		return result
+		return
 	
 	dir.list_dir_begin()
 	var file_name := dir.get_next()
 	
 	while file_name != "":
-		_process_file(path, file_name, key_property, result)
+		if file_name == "." or file_name == "..":
+			file_name = dir.get_next()
+			continue
+		
+		var full_path := path.path_join(file_name)
+		
+		if dir.current_is_dir():
+			_scan_dir_recursive(full_path, key_property, result)
+		else:
+			_process_file(path, file_name, key_property, result)
+		
 		file_name = dir.get_next()
 	
 	dir.list_dir_end()
-	return result
 
 static func _process_file(base_path: String, file_name: String, key_property: String, result: Dictionary) -> void:
-	# Directory
-	if file_name.ends_with("/") or file_name.ends_with("\\"):
-		return
-	
-	# Not resource
 	if not (file_name.ends_with(".tres") or file_name.ends_with(".res")):
 		return
 	
@@ -38,5 +45,5 @@ static func _process_file(base_path: String, file_name: String, key_property: St
 	if not res.get(key_property):
 		push_error("Resource %s missing '%s'" % [full_path, key_property])
 		return
-
+	
 	result[res.get(key_property)] = res
