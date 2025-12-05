96f1151f11e0068d25c347ca8e4ca718928f9300
Bokeher <bokeher47@gmail.com>
2025-12-05 19:58:18 +0100

    Add data validation for StageDatabase

--- scripts/classes/databases/StageDatabase.gd
+++ scripts/classes/databases/StageDatabase.gd
@@ -6,5 +6,16 @@ func _init() -> void:
 	_dir_path = "res://assets/resources/stages/"
 	_data_type = "Stage"
 
+func _ready() -> void:
+	super._ready()
+	is_data_valid()
+
 func get_by_id(id: int) -> StageData:
 	return super.get_by_id(id)
+
+func is_data_valid() -> void:
+	for stage_id: int in _data:
+		var stage: StageData = _data[stage_id]
+		if not stage.are_spawn_entries_valid():
+			print("Stage '%s' has invalid data" % stage.name)
+	
--- scripts/classes/resource_classes/StageData.gd
+++ scripts/classes/resource_classes/StageData.gd
@@ -5,3 +5,10 @@ class_name StageData
 @export var name: String
 @export var texture: Texture2D
 @export var spawn_entries: Array[SpawnEntry]
+
+func are_spawn_entries_valid() -> bool:
+	var sum: float = 0.0
+	for entry in spawn_entries:
+		sum += entry.spawn_chance
+	
+	return abs(sum - 1.0) <= 0.001
