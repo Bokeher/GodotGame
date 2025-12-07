2ccf4f6f6caa6674f052cf3a5f9175cea5962492
Bokeher <bokeher47@gmail.com>
2025-12-07 20:23:17 +0100

    Add PlayerData

---
+++ scripts/classes/PlayerData.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name PlayerData
+
+@export var base_max_health: int = 10
+@export var base_defense: int = 0
+@export var base_health_regen: float = 0
+
+@export var base_attack_damage: int = 1
+@export var base_attack_interval: float = 0.5
+@export var base_crit_chance: float = 0.00
+@export var base_crit_damage_multiplier: float = 2.00
