3deb7d37f6a89432daf33aa622714149e996706e
Bokeher <bokeher47@gmail.com>
2025-12-09 20:37:40 +0100

    Add damage handling and death signals

--- scripts/classes/instance_classes/EnemyInstance.gd
+++ scripts/classes/instance_classes/EnemyInstance.gd
@@ -4,6 +4,20 @@ class_name EnemyInstance
 @export var enemy_data: EnemyData
 @export var health: int
 
+signal hit(damage: int)
+signal died(enemy: EnemyInstance)
+
 func _init(enemy_data: EnemyData) -> void:
 	self.enemy_data = enemy_data
 	health = enemy_data.health
+
+func deal_damage(damage: int) -> void:
+	health -= damage
+	
+	hit.emit(damage)
+	
+	if health <= 0:
+		die()
+
+func die() -> void:
+	died.emit(self)
