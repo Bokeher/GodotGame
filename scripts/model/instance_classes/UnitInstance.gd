13845e86b3a12125444d6de90627b02543095423
Bokeher <bokeher47@gmail.com>
2025-12-25 13:50:35 +0100

    Add get_attack_damage() and get_damage_reduction() to UnitInstance

--- scripts/model/instance_classes/UnitInstance.gd
+++ scripts/model/instance_classes/UnitInstance.gd
@@ -17,5 +17,13 @@ func receive_damage(amount: int) -> void:
 	if health == 0:
 		died.emit(self)
 
+func get_damage_reduction(incoming_damage: int) -> int:
+	return 0
+
+# ABSTRACT METHOD
+func get_attack_damage() -> int:
+	assert(false, "UnitInstance.get_attack_damage() must be overridden")
+	return 0
+
 func get_hp_str() -> String:
 	return "%d / %d HP" % [health, max_health]
