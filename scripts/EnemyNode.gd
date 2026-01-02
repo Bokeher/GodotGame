867821d878e2a880eb6be433991ad1d9fcdacc02
Bokeher <bokeher47@gmail.com>
2026-01-02 19:49:45 +0100

    Display enemy texture and name correctly (also handle null enemy)

--- scripts/EnemyNode.gd
+++ scripts/EnemyNode.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 func _ready() -> void:
 	GameManager.enemy_changed.connect(on_enemy_changed)
-	enemy.health_changed.connect(update_health_ui)
+	enemy.health_changed.connect(update_enemy_health_ui)
 	
 	GameManager.sync_enemy()
 	enemy.sync()
@@ -19,17 +19,34 @@ func on_enemy_changed(new_enemy: EnemyInstance):
 	if enemy:
 		enemy.health_changed.connect(on_health_changed)
 	
-	update_health_ui()
+	update_enemy_ui()
+	update_enemy_health_ui()
 
-func update_health_ui() -> void:
+func update_enemy_ui() -> void:
+	if enemy == null:
+		$EnemyBody.texture_normal = null
+		$EnemyName.text = ""
+		return
+	
+	$EnemyName.text = enemy.enemy_data.name
+	$EnemyBody.texture_normal = enemy.enemy_data.texture
+
+func update_enemy_health_ui() -> void:
+	if enemy == null:
+		$HealthBar/EnemyHealthLabel.text = ""
+		return
+	
 	$HealthBar.max_value = enemy.max_health
 	$HealthBar.value = enemy.health
 	$HealthBar/EnemyHealthLabel.text = enemy.get_hp_str()
 
 func on_health_changed() -> void:
-	update_health_ui()
+	update_enemy_health_ui()
 
 func _on_enemy_body_pressed() -> void:
+	if enemy == null:
+		return
+	
 	GameManager.combat_manager.deal_damage(
 		GameManager.player, GameManager.enemy
 	)
