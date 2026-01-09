3df30e8a001359547bf9bd2ec72f6b88cb724778
Bokeher <bokeher47@gmail.com>
2026-01-09 06:19:34 +0100

    Redo signals in UnitInstance

--- scripts/model/instance_classes/UnitInstance.gd
+++ scripts/model/instance_classes/UnitInstance.gd
@@ -4,15 +4,20 @@ class_name UnitInstance
 @export var health: int
 @export var max_health: int
 
-signal health_changed()
-signal died(instance)
+signal health_changed(old_value: int, new_value: int)
+signal damaged(amount: int)
+signal died(instance: UnitInstance)
 
 func receive_damage(amount: int) -> void:
-	if amount <= 0:
+	if amount <= 0 or health <= 0:
 		return
 	
+	var old_health := health
 	health = max(health - amount, 0)
-	health_changed.emit()
+	
+	if health < old_health:
+		damaged.emit(old_health - health)
+		health_changed.emit(old_health, health)
 	
 	if health == 0:
 		died.emit(self)
