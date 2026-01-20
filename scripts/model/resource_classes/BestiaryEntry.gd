46db6474d22c533fecf370be6a30f8be9cd60ff3
Bokeher <bokeher47@gmail.com>
2026-01-20 20:46:19 +0100

    Fix comment

--- scripts/model/resource_classes/BestiaryEntry.gd
+++ scripts/model/resource_classes/BestiaryEntry.gd
@@ -1,4 +1,4 @@
 extends Resource
 
 @export var times_slayed: int
-@export var items_dropped: Dictionary[int, bool] # <id, bool>
+@export var items_dropped: Dictionary[int, bool] # <id, has_dropped>
