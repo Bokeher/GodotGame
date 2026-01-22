de2344af20aaa5a777bd97cef58f7cf2424376a8
Bokeher <bokeher47@gmail.com>
2026-01-22 20:57:55 +0100

    Use combat_controller

--- scripts/EnemyNode.gd
+++ scripts/EnemyNode.gd
@@ -60,6 +60,5 @@ func _on_enemy_body_pressed() -> void:
 	if enemy == null:
 		return
 	
-	GameManager.damage_resolver.deal_damage(
-		GameManager.player, GameManager.enemy
-	)
+	GameManager.combat_controller.player_attack.try_attack(enemy)
+	GameManager.combat_controller.enemy_attack.start_auto(GameManager.player)
