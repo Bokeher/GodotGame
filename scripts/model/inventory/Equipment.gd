6fe1083dbb2d3266d4b342b209d5abd72c8b89bd
Bokeher <bokeher47@gmail.com>
2026-02-08 14:25:21 +0100

    Add EquipmentSlotId to differentiate multiple slots of the same type

--- scripts/model/inventory/Equipment.gd
+++ scripts/model/inventory/Equipment.gd
@@ -1,13 +1,23 @@
 extends Object
 class_name Equipment
 
-var slots: Dictionary[ItemData.ItemType, ItemData] = {
-	ItemData.ItemType.RING: null,
-	ItemData.ItemType.CHEST: null
+signal item_equipped(slot: EquipmentSlotId, item: ItemData)
+signal item_unequipped(slot: EquipmentSlotId)
+signal equipment_changed(slot: EquipmentSlotId)
+
+enum EquipmentSlotId {
+	RING1,
+	RING2,
+	CHEST
+}
+
+var slots: Dictionary[EquipmentSlotId, ItemData] = {
+	EquipmentSlotId.RING1: null,
+	EquipmentSlotId.CHEST: null
 }
 
 func equip_item(item: ItemData):
 	slots[item.type] = item
 
-func unequip_item(type: ItemData.ItemType):
+func unequip_item(type: EquipmentSlotId):
 	slots[type] = null
