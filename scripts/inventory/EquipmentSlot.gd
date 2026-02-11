0c09df0d08870f963db2aab5f3ba46a8c088e18f
Bokeher <bokeher47@gmail.com>
2026-02-11 20:55:58 +0100

    Add EquipmentSlot.gd

---
+++ scripts/inventory/EquipmentSlot.gd
@@ -0,0 +1,25 @@
+extends Control
+class_name EquipmentSlot
+
+@export var item: ItemData = null
+@export var equipment_slot_id: Equipment.EquipmentSlotId
+var is_selected: bool
+
+signal pressed(item: Equipment.EquipmentSlotId)
+
+func setup(slot_id: Equipment.EquipmentSlotId, is_selected_ = false) -> void:
+	equipment_slot_id = slot_id
+	is_selected = is_selected_
+	
+	GameManager.player.inventory.equipment.equipment_changed.connect(_on_equipment_change)
+
+func _on_equipment_change(slot_id: Equipment.EquipmentSlotId) -> void:
+	if slot_id != equipment_slot_id:
+		return
+	
+	item = GameManager.player.inventory.equipment.get_item(slot_id)
+	
+	update_ui()
+
+func update_ui() -> void:
+	$SlotTexture.texture_normal = item.texture
