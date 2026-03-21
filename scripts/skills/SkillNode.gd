b57032444d5f5761751abed85ffb623a47a94d34
Bokeher <bokeher47@gmail.com>
2026-03-21 20:29:20 +0100

    Remove PlayerSkills from SkillNode to reduce tight coupling

--- scripts/skills/SkillNode.gd
+++ scripts/skills/SkillNode.gd
@@ -2,17 +2,15 @@ extends Control
 class_name SkillNode
 
 @export var skill: SkillData
-var _player_skills: PlayerSkills
 
 signal pressed(skill: SkillData)
 signal hovered(skill: SkillData)
 signal hover_exited()
 
-func setup(skill_: SkillData, player_skills_: PlayerSkills) -> void:
+func setup(skill_: SkillData) -> void:
 	skill = skill_
-	_player_skills = player_skills_
 	
-	update_level_label()
+	update_level_label(0)
 	update_skill_image()
 
 func _on_texture_button_mouse_entered() -> void:
@@ -26,10 +24,8 @@ func _on_texture_button_mouse_exited() -> void:
 func _on_texture_button_pressed() -> void:
 	pressed.emit(skill)
 
-func update_level_label() -> void:
-	var curr_level: int = _player_skills.get_level(skill.id)
-	
-	$SkillLevelLabel.text = str(curr_level) + " / " + str(skill.max_level)
+func update_level_label(skill_level: int) -> void:
+	$SkillLevelLabel.text = str(skill_level) + " / " + str(skill.max_level)
 
 func update_skill_image() -> void:
 	$SkillButton.texture_normal = skill.texture
