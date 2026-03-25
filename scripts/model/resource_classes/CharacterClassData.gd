65b9e7afb8048f954f48dc39714cb9bffa3c94fe
Bokeher <bokeher47@gmail.com>
2026-03-25 20:37:17 +0100

    Add CharacterClassData

---
+++ scripts/model/resource_classes/CharacterClassData.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name CharacterClassData
+
+@export var class_type: Enums.CharacterClass
+@export var display_name: String
+@export var description: String
+@export var texture: Texture2D
+
+@export var skills: Array[SkillData]
