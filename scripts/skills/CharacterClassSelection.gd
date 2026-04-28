e67c7a3697c41e96f815df8a1bdaac223749ebcd
Bokeher <bokeher47@gmail.com>
2026-04-28 21:04:11 +0200

    refactor(character-selection): replace id arithmetic with index-based navigation

--- scripts/skills/CharacterClassSelection.gd
+++ scripts/skills/CharacterClassSelection.gd
@@ -4,6 +4,8 @@ extends Control
 
 var current_char_id: int = Enums.CharacterClass.WARRIOR
 
+@onready var classes: Array = Enums.CharacterClass.values().filter(func(c: int) -> int: return c >= 0 ) 
+
 func _ready() -> void:
 	update_char(current_char_id)
 
@@ -12,20 +14,15 @@ func update_char(id: int) -> void:
 	char_selection_view.set_char_class(CharacterClassDatabase.get_by_id(id))
 
 func _on_previous_button_pressed() -> void:
-	print(current_char_id)
-	print(Enums.CharacterClass.has(current_char_id))
-	
-	current_char_id -= 1
-	
-	if !(current_char_id in Enums.CharacterClass.values()) or current_char_id == -1:
-		current_char_id = Enums.CharacterClass.values().max()
+	var index: int = classes.find(current_char_id)
+	index = (index - 1 + classes.size()) % classes.size()
+	current_char_id = classes[index]
 	
 	update_char(current_char_id)
 
 func _on_next_button_pressed() -> void:
-	current_char_id += 1
-	
-	if !(current_char_id in Enums.CharacterClass.values()) or current_char_id == -1:
-		current_char_id = 0
+	var index: int = classes.find(current_char_id)
+	index = (index + 1) % classes.size()
+	current_char_id = classes[index]
 	
 	update_char(current_char_id)
